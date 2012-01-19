19ad6b894085c7ee3d74ebacc8cf3dec5cf4f536
João Matias <joaocmatias@gmail.com>
2012-01-19 03:19:52 +0000

    3º Trabalho completo

---
+++ Trabalho3/src/Markov/ModeloPDM.java
@@ -0,0 +1,15 @@
+package Markov;
+import java.util.Set;
+
+import espest.OperadorGeral;
+import espest.Transicao;
+
+
+public interface ModeloPDM<E> {
+	
+	double t(Transicao<E> trans);
+	double r(Transicao<E> trans);
+	Set<E> s();
+	Set<OperadorGeral<E>> o(E s);
+//	void pintar();
+}
